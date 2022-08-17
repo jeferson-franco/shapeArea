51cf981e741171f6eb63e93971db5f75171dd5ef
jefersonfranco <jefersonfranco@protonmail.com>
2022-08-17 17:21:21 -0300

    feat: create shapeArea.js

---
+++ src/shapeArea.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    return n * n + (n - 1) * (n - 1);
+}
+
+module.exports = solution;
+
+// npm init -y
+// npm i jest-cli -g
+// package.json: "scripts": { "test": "jest" }
+// npm test shapeArea
