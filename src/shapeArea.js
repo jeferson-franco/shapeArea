de726a1f522a2b4057ef8ee48b02dcf7670f2b1d
jefersonfranco <jefersonfranco@protonmail.com>
2022-08-19 14:50:12 -0300

    update shapeArea.js

--- src/shapeArea.js
+++ src/shapeArea.js
@@ -8,3 +8,14 @@ module.exports = solution;
 // npm i jest-cli -g
 // package.json: "scripts": { "test": "jest" }
 // npm test shapeArea
+
+// alternative solution
+// function solution(n) {
+//     return Math.pow(n, 2) + Math.pow(n - 1, 2);
+// }
+
+// alternative solution
+// function solution(n) {
+//    if (n === 1) return 1;
+//    return solution(n - 1) + 4 * (n - 1);
+// }
